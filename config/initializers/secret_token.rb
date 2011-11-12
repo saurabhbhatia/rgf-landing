65ca8a8e71969a277d04194325eb90addaa053de
railosmith <railosmith@railosmith.(none)>
2011-11-12 23:53:27 +0530

    updated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RealgoodfoodLandingApp::Application.config.secret_token = '15f8e732312d41312487a32c914639de0885a6b3375233e97c6c295bcfca76e98b2a306743e4cb009448cabb4b2bed040fbafc39d08df292bba8c7cd5afa5dc5'
